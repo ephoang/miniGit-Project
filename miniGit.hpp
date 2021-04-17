b87ef8472f77f1e5846ce382330ebafde0525d2a
Grahkay <58275048+Grahkay@users.noreply.github.com>
2021-04-16 19:34:37 -0600

    Update miniGit.hpp

--- miniGit.hpp
+++ miniGit.hpp
@@ -1,1 +1,30 @@
+#ifndef MINIGIT
+#define MINIGIT
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstring>
+#include <sstream>
+using namespace std;
 
+struct doublyNode{
+    int commitNumber;
+    singlyNode * head;
+    doublyNode * previous;
+    doublyNode * next;
+};
+
+struct singlyNode{
+    string fileName;
+    string fileVersion;
+    singlyNode * next;
+};
+
+class miniGit{
+    private:
+
+    public:
+        
+};
+
+#endif
