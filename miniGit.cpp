1a94bc396443a0a08c305d003ca9062e8246d0a4
ephoang <82679333+ephoang@users.noreply.github.com>
2021-04-25 15:02:02 -0600

    Update miniGit.cpp

--- miniGit.cpp
+++ miniGit.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-bool miniGit::search(singlyNode* head, string file)
+bool miniGit::search(string file)
 {
     singlyNode* curr = head;
     while(curr != NULL)
@@ -25,9 +25,33 @@ void miniGit::RemoveRepostitory(string repname)
 {
 }
 
-void miniGit::AddFile(string filename)
+string filePrompt()
 {
+    //Prompt user to enter file name
+    string fileName;
+    cout "Enter the file name: " << endl;
+    cin >> fileName;
+    return fileName;
+}    
+
+void miniGit::AddFile()
+{
+    //Prompt user to enter file name
+    //Check whether the file with the given name exists in the current directory. If not, keep prompting the user to enter a valid vile name
+    bool exists = false;
+    while (exists == false)
+    {
+        string fileName;
+        fileName = filePrompt();
+        exists = search(head, fileName);
+        if (exists == false)
+        {
+            cout << "File does not exist in directory." << endl;
+        }
+    }
     
+    //The SLL is checked to see whether the file has already been added. A file by the same name cannot be added twice
+    //A new SLL node gets added containing the name of the input file, the name of the repository file, as well as a pointer to the next node. 
 }
 
 void miniGit::RemoveFile(singlyNode* node, string filename)
